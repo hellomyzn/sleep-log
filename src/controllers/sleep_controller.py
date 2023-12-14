e25465bc8e5d25f94471a56e9c5abeee580a4ca0
hellomyzn <em711th@icloud.com>
2023-12-14 20:41:26 +0900

    update: controler

--- src/controllers/sleep_controller.py
+++ src/controllers/sleep_controller.py
@@ -30,21 +30,29 @@ class SleepController(object):
         Returns:
             None: _description_
         """
-        info("start to add sleep log")
         cr = CsvSleepRepository()
         ssc = SleepService(cr)
+
         sleep_data = ssc.get()
+        sleep_data_from_csv = ssc.all()
 
-        new_data = ssc.new(sleep_data)
+        new_data = sleep_data
+        next_id = 1
+        if sleep_data_from_csv:
+            latest_datetime = sleep_data_from_csv[-1]["bedtime_start"]
+            next_id = int(sleep_data_from_csv[-1]["id"]) + 1
+            new_data = ssc.filter_data_by_datetime(sleep_data, latest_datetime)
 
-        # remove unnecessary data or separalate
-        # contributors
-        # heart_rate
-        # hrv
-        # readiness
+        info(f"new data num: {len(new_data)}")
 
         if new_data:
-            new_data_with_id = ssc.put_id(new_data)
-            ssc.add(new_data_with_id)
-        info("finish to add sleep log")
+            new_data_with_id = ssc.put_id(new_data, next_id)
+
+            # remove unnecessary data
+            contributors = ssc.extract_from_sleep_data(new_data_with_id, "contributors")
+            heart_rate = ssc.extract_from_sleep_data(new_data_with_id, "heart_rate")
+            hrv = ssc.extract_from_sleep_data(new_data_with_id, "hrv")
+            readiness = ssc.extract_from_sleep_data(new_data_with_id, "readiness")
+
+            # ssc.add(new_data_with_id)
         return None
